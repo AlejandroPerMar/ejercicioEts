4e75f7438045e837a39cde541d00dc736d62976d
Alejandro Pérez Martín <91550955+AlejandroPerMar@users.noreply.github.com>
2022-03-16 17:25:53 +0000

    Se añade el proyecto Padre y el modulo. close #1
    En este se añade todo el proyecto con las clases  y todo lo requerido

---
+++ ejercicioEts/area/src/main/java/es.iespuerto.area/AlejandroPerezMartin.java
@@ -0,0 +1,42 @@
+package es.iespuerto.area;
+
+public class AlejandroPerezMartin {
+
+    String tipo;
+    Integer valor1;
+    Integer valor2;
+
+    /**
+     * Constructor por defecto
+     */
+    public AlejandroPerezMartin() {
+
+    }
+
+    /**
+     * Constructor de un parametro
+     * @param tipo de la clase
+     */
+    public AlejandroPerezMartin(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * Constructor de dos valores
+     * @param valor1
+     * @param valor2
+     */
+    public AlejandroPerezMartin(Integer valor1, Integer valor2) {
+        this.valor1 = valor1;
+        this.valor2 = valor2;
+    }
+
+    /**
+     *  Metodo para calcular Area
+     * @param valor1 del metodo
+     * @param valor2 del metodo
+     */
+    public void CalculoArea (Integer valor1 , Integer valor2){
+
+    }
+}
